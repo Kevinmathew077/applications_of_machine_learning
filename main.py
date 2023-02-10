9e1863a0996d72e38c53e241e3356e3591899c0e
jijinraj <jijinraj401@gmail.com>
2023-02-10 11:53:56 +0000

    Keep file operations seperated into another file (fileio.py) , so we can keep our opencv.py clean
    Co-Authored-By: Kevinmathew077 <kevinmathewparath007@gmail.com>
    Co-Authored-By: RohitRoby <rohitlancing12@gmail.com>

--- main.py
+++ main.py
@@ -1,22 +1,31 @@
 import utils.opencv as cv
+import utils.fileio as fileio
 import asyncio
 import time
+import os
 
 
 # first checkout directories
 
 
-async def main():
 
+
+async def main():
+    
+    root_directory = os.getcwd()
+    print(root_directory)
+    fileio.check_paths(root_directory)
+    
+        
     start = time.time()
 
     # Directory containing frames
     directory = cv.working_directory
 
-    cv.extract_frames("input/vd.mp4")
+    # cv.extract_frames("input/vd.mp4")
 
     # Process the directory asynchronously
-    await cv.process_directory(directory)
+    # await cv.process_directory(directory)
 
     end = time.time()
 
---
+++ utils/fileio.py
@@ -0,0 +1,43 @@
+import os
+
+
+def check_paths(root_directory):
+
+    root_directory = root_directory
+    working_directory = "{}/working/".format(root_directory)
+    json_directory = "{}/working/json/".format(root_directory)
+    face_directory = "{}/working/faces/".format(root_directory)
+    frame_directory = "{}/working/frames".format(root_directory)
+
+    if not check_dir(working_directory):
+        create_dir(working_directory)
+        print("Directory Created At {}".format(working_directory))
+    else:
+        print("Directory Already Exist")
+
+    if not check_dir(json_directory):
+        create_dir(json_directory)
+        print("Directory Created At {}".format(json_directory))
+    else:
+        print("Directory Already Exist")
+
+    if not check_dir(face_directory):
+        create_dir(face_directory)
+        print("Directory Created At {}".format(face_directory))
+    else:
+        print("Directory Already Exist")
+
+    if not check_dir(frame_directory):
+        create_dir(frame_directory)
+        print("Directory Created At {}".format(frame_directory))
+    else:
+        print("Directory Already Exist")
+
+
+def create_dir(directory):
+    os.makedirs(directory)
+
+
+def check_dir(path):
+    path_exist = os.path.exists(path)
+    return path_exist
